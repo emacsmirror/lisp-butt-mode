2b719baf0ccba79e28fcb3c2633c4849d976ac23
Marco Wahl <marcowahlsoft@gmail.com>
2021-02-15 23:06:57 +0100

    ^update version

--- lisp-butt-mode.el
+++ lisp-butt-mode.el
@@ -11,7 +11,7 @@
 ;; Author: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Maintainer: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Created: [2019-07-11]
-;; Version: 2.0.3
+;; Version: 2.0.4
 ;; Package-Requires: ((emacs "25"))
 ;; Keywords: lisp
 ;; URL: https://gitlab.com/marcowahl/lisp-butt-mode
